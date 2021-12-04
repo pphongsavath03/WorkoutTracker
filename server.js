e5cda81dc8b41b918eb597e85f5fc5628a21ac7c
pphong <pphongsavath03@gmail.com>
2021-12-04 18:04:56 -0500

    added server

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3001;
+const db = require("./Models");
+
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+require("./routes/html-routes.js")(app);
+require("./routes/api-routes.js")(app);
+
+mongoose.connect(
+    process.env.MONGODB_URI || "mongodb://localhost/custommethoddb", { useNewUrlParser: true }
+);
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+});
